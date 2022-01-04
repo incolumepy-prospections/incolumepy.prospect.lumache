ff059004e26c134533dc1ae9f94f73bdc8b8ed52
Britodfbr <britodfbr@gmail.com>
2022-01-04 14:01:34 -0300

    Enabling a built-in extension

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,6 +33,7 @@ release = root.joinpath(*'incolumepy prospect lumache version.txt'.split()).read
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.duration',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
